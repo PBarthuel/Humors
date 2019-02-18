4291c793f8d9ffbad94b198317a6b03bb6daf185
barthuel <paulbarthuel@hotmail.fr>
2019-02-18 16:13:38 +0100

    Ajout de la possibilite de cliquer sur le bouton commentaire et affichage d'un AlertDialog

--- app/src/main/java/paul/barthuel/humors/MainActivity.java
+++ app/src/main/java/paul/barthuel/humors/MainActivity.java
@@ -1,11 +1,14 @@
 package paul.barthuel.humors;
 
+import android.content.DialogInterface;
 import android.support.v4.view.GestureDetectorCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
@@ -18,6 +21,8 @@ public class MainActivity extends AppCompatActivity implements
     ImageView smiley;
     RelativeLayout mainRelativeLayout;
     int smileyCount;
+    ImageView mCommentButton;
+    ImageView mHistoryButton;
 
     // Called when the activity is first created.
     @Override
@@ -28,10 +33,27 @@ public class MainActivity extends AppCompatActivity implements
         smiley.setImageResource(R.drawable.smiley_super_happy);
         mainRelativeLayout = findViewById(R.id.main_rl_parent);
         mainRelativeLayout.setBackgroundResource(R.color.banana_yellow);
+        mCommentButton = findViewById(R.id.main_iv_comment);
+        mHistoryButton = findViewById(R.id.main_iv_history);
         // Instantiate the gesture detector with the
         // application context and an implementation of
         // GestureDetector.OnGestureListener
         mDetector = new GestureDetectorCompat(this, this);
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        mCommentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // End the activity
+                    }
+                })
+                        .show();
+            }
+        });
     }
 
     @Override
