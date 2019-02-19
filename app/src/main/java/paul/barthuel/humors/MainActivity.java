9472ef2838ddaf64fe211339ee4a8151a966a3cc
barthuel <paulbarthuel@hotmail.fr>
2019-02-19 08:34:34 +0100

    création de l'activité HistoryActivity

--- app/src/main/java/paul/barthuel/humors/MainActivity.java
+++ app/src/main/java/paul/barthuel/humors/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity implements
                 builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        // End the activity
+
                     }
                 })
                         .show();
