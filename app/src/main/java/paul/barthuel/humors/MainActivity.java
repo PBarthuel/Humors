0b3f018d326f37e980895f17676a7c41a1e437a7
barthuel <paulbarthuel@hotmail.fr>
2019-02-19 09:42:51 +0100

    ajout d'un bouton Cancel sur la demande de commentaire

--- app/src/main/java/paul/barthuel/humors/MainActivity.java
+++ app/src/main/java/paul/barthuel/humors/MainActivity.java
@@ -1,6 +1,7 @@
 package paul.barthuel.humors;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -9,7 +10,9 @@ import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity implements
@@ -17,7 +20,7 @@ public class MainActivity extends AppCompatActivity implements
 
     private static final String DEBUG_TAG = "Gestures";
     private GestureDetectorCompat mDetector;
-
+    public static final int GAME_ACTIVITY_REQUEST_CODE = 1;
     ImageView smiley;
     RelativeLayout mainRelativeLayout;
     int smileyCount;
@@ -45,13 +48,34 @@ public class MainActivity extends AppCompatActivity implements
         mCommentButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final EditText input = new EditText(MainActivity.this);
+                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT);
+                input.setLayoutParams(lp);
+                builder.setView(input);
+                builder.setTitle("Enter your commentary :D");
                 builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
 
                     }
                 })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        })
                         .show();
+            }
+        });
+
+        mHistoryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent historyActivityIntent = new Intent(MainActivity.this, HistoryActivity.class);
+                startActivityForResult(historyActivityIntent, GAME_ACTIVITY_REQUEST_CODE);
             }
         });
     }
@@ -74,43 +98,43 @@ public class MainActivity extends AppCompatActivity implements
     public boolean onFling(MotionEvent event1, MotionEvent event2,
                            float velocityX, float velocityY) {
         Log.d(DEBUG_TAG, "onFling: " + velocityY);
-        if(smileyCount == 0) {
-            if(velocityY < 0) {
+        if (smileyCount == 0) {
+            if (velocityY < 0) {
                 smiley.setImageResource(R.drawable.smiley_happy);
                 mainRelativeLayout.setBackgroundResource(R.color.light_sage);
                 smileyCount = 1;
             }
-        } else if(smileyCount == 1) {
-            if(velocityY < 0) {
+        } else if (smileyCount == 1) {
+            if (velocityY < 0) {
                 smiley.setImageResource(R.drawable.smiley_normal);
                 mainRelativeLayout.setBackgroundResource(R.color.cornflower_blue_65);
                 smileyCount = 2;
-            }else {
+            } else {
                 smiley.setImageResource(R.drawable.smiley_super_happy);
                 mainRelativeLayout.setBackgroundResource(R.color.banana_yellow);
                 smileyCount = 0;
             }
-        }else if(smileyCount == 2){
-            if(velocityY < 0) {
+        } else if (smileyCount == 2) {
+            if (velocityY < 0) {
                 smiley.setImageResource(R.drawable.smiley_disappointed);
                 mainRelativeLayout.setBackgroundResource(R.color.warm_grey);
                 smileyCount = 3;
-            }else {
+            } else {
                 smiley.setImageResource(R.drawable.smiley_happy);
                 mainRelativeLayout.setBackgroundResource(R.color.light_sage);
                 smileyCount = 1;
             }
-        }else if(smileyCount == 3) {
-            if(velocityY < 0) {
+        } else if (smileyCount == 3) {
+            if (velocityY < 0) {
                 smiley.setImageResource(R.drawable.smiley_sad);
                 mainRelativeLayout.setBackgroundResource(R.color.faded_red);
                 smileyCount = 4;
-            }else {
+            } else {
                 smiley.setImageResource(R.drawable.smiley_normal);
                 mainRelativeLayout.setBackgroundResource(R.color.cornflower_blue_65);
                 smileyCount = 2;
             }
-        }else if(smileyCount == 4) {
+        } else if (smileyCount == 4) {
             if (velocityY > 0) {
                 smiley.setImageResource(R.drawable.smiley_disappointed);
                 mainRelativeLayout.setBackgroundResource(R.color.warm_grey);
