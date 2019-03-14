c39d7b65c0483e1e604f257b3c97c0fe4f0506a3
barthuel <paulbarthuel@hotmail.fr>
2019-03-14 14:54:56 +0100

    gestion de la selection d'humeur terminer grace aux enums

--- app/src/main/java/paul/barthuel/humors/MainActivity.java
+++ app/src/main/java/paul/barthuel/humors/MainActivity.java
@@ -2,6 +2,7 @@ package paul.barthuel.humors;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +15,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.jakewharton.threetenabp.AndroidThreeTen;
 
@@ -22,12 +24,16 @@ public class MainActivity extends AppCompatActivity implements
 
     private static final String DEBUG_TAG = "Gestures";
     private GestureDetectorCompat mDetector;
-    public static final int GAME_ACTIVITY_REQUEST_CODE = 1;
+    public static final int HISTORY_ACTIVITY_REQUEST_CODE = 1;
+
     ImageView smiley;
     RelativeLayout mainRelativeLayout;
     Mood currentMood = Mood.SUPER_HAPPY;
     ImageView mCommentButton;
     ImageView mHistoryButton;
+    int fesse = 0;
+
+
 
     // Called when the activity is first created.
     @Override
@@ -47,6 +53,8 @@ public class MainActivity extends AppCompatActivity implements
 
         mCommentButton = findViewById(R.id.main_iv_comment);
         mHistoryButton = findViewById(R.id.main_iv_history);
+
+        
 
         // Instantiate the gesture detector with the
         // application context and an implementation of
@@ -75,7 +83,6 @@ public class MainActivity extends AppCompatActivity implements
                         .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-
                             }
                         })
                         .show();
@@ -86,7 +93,7 @@ public class MainActivity extends AppCompatActivity implements
             @Override
             public void onClick(View v) {
                 Intent historyActivityIntent = new Intent(MainActivity.this, HistoryActivity.class);
-                startActivityForResult(historyActivityIntent, GAME_ACTIVITY_REQUEST_CODE);
+                startActivityForResult(historyActivityIntent, HISTORY_ACTIVITY_REQUEST_CODE);
             }
         });
     }
@@ -110,11 +117,18 @@ public class MainActivity extends AppCompatActivity implements
                            float velocityX, float velocityY) {
         Log.d(DEBUG_TAG, "onFling: " + velocityY);
         boolean getHappier = velocityY < 0;
-        if(getHappier){
-            currentMood = Mood.values()[currentMood.ordinal()-1];
-        }else  {
-            currentMood = Mood.values()[currentMood.ordinal()+1];
+        int ordinalMood;
+        if (getHappier) {
+            ordinalMood = currentMood.ordinal()+1;
+        }else {
+            ordinalMood = currentMood.ordinal()-1;
         }
+        if (ordinalMood < 0) {
+            ordinalMood = 0;
+        }else if (ordinalMood > Mood.values().length - 1){
+            ordinalMood = Mood.values().length - 1;
+        }
+        currentMood = Mood.values()[ordinalMood];
         smiley.setImageResource(currentMood.getDrawableRes());
         mainRelativeLayout.setBackgroundResource(currentMood.getColorRes());
         return true;
